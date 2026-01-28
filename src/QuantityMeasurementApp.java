97c370e839fe067a7f46ae8862a21aa518e850e7
bhumesh848 <bhumesh.ranjaneb598@gmail.com>
2026-01-28 11:46:04 +0530

    Implemented UC-1

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -18,6 +18,7 @@ public class QuantityMeasurementApp {
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
+
             if (obj == null || getClass() != obj.getClass()) return false;
             Feet other = (Feet) obj;
 
