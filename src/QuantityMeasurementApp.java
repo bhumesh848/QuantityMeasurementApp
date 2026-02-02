03c470d13d48e951e4079cc76f41ff1a53827b80
bhumesh848 <bhumesh.ranjaneb598@gmail.com>
2026-02-02 11:13:56 +0530

    Implemented the u3

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,91 +1,31 @@
 public class QuantityMeasurementApp {
 
-    /* ===================== FEET CLASS ===================== */
-    public static class Feet {
+    public static boolean checkEquality(
+            double value1, LengthUnit unit1,
+            double value2, LengthUnit unit2) {
 
-        private final double value;
+        QuantityLength length1 = new QuantityLength(value1, unit1);
+        QuantityLength length2 = new QuantityLength(value2, unit2);
 
-        public Feet(double value) {
-            validate(value);
-            this.value = value;
-        }
-
-        private void validate(double value) {
-            if (Double.isNaN(value) || Double.isInfinite(value)) {
-                throw new IllegalArgumentException("Feet value must be numeric");
-            }
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-
-            Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value) == 0;
-        }
-
-        @Override
-        public int hashCode() {
-            return Double.hashCode(value);
-        }
+        return length1.equals(length2);
     }
-
-    /* ===================== INCHES CLASS ===================== */
-    public static class Inches {
-
-        private final double value;
-
-        public Inches(double value) {
-            validate(value);
-            this.value = value;
-        }
-
-        private void validate(double value) {
-            if (Double.isNaN(value) || Double.isInfinite(value)) {
-                throw new IllegalArgumentException("Inches value must be numeric");
-            }
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-
-            Inches other = (Inches) obj;
-            return Double.compare(this.value, other.value) == 0;
-        }
-
-        @Override
-        public int hashCode() {
-            return Double.hashCode(value);
-        }
-    }
-
-    /* ===================== STATIC UTILITY METHODS ===================== */
-
-    public static boolean checkFeetEquality(double value1, double value2) {
-        Feet feet1 = new Feet(value1);
-        Feet feet2 = new Feet(value2);
-        return feet1.equals(feet2);
-    }
-
-    public static boolean checkInchesEquality(double value1, double value2) {
-        Inches inch1 = new Inches(value1);
-        Inches inch2 = new Inches(value2);
-        return inch1.equals(inch2);
-    }
-
-    /* ===================== MAIN METHOD ===================== */
 
     public static void main(String[] args) {
 
-        System.out.println("Input: 1.0 inch and 1.0 inch");
-        System.out.println("Output: Equal (" + checkInchesEquality(1.0, 1.0) + ")");
+        System.out.println("Input: 12 inch and 1 foot");
+        System.out.println("Output: Equal (" +
+                checkEquality(12.0, LengthUnit.INCH, 1.0, LengthUnit.FEET) + ")");
 
         System.out.println();
 
-        System.out.println("Input: 1.0 ft and 1.0 ft");
-        System.out.println("Output: Equal (" + checkFeetEquality(1.0, 1.0) + ")");
+        System.out.println("Input: 1 foot and 1 foot");
+        System.out.println("Output: Equal (" +
+                checkEquality(1.0, LengthUnit.FEET, 1.0, LengthUnit.FEET) + ")");
+
+        System.out.println();
+
+        System.out.println("Input: 1 inch and 1 inch");
+        System.out.println("Output: Equal (" +
+                checkEquality(1.0, LengthUnit.INCH, 1.0, LengthUnit.INCH) + ")");
     }
 }
