a99fbff00e65ea73e75b1e999f1caf661c01426b
bhumesh848 <bhumesh.ranjaneb598@gmail.com>
2026-02-02 11:25:41 +0530

    Implemented uc4 for ExtendedUnitSupport

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -4,28 +4,27 @@ public class QuantityMeasurementApp {
             double value1, LengthUnit unit1,
             double value2, LengthUnit unit2) {
 
-        QuantityLength length1 = new QuantityLength(value1, unit1);
-        QuantityLength length2 = new QuantityLength(value2, unit2);
+        QuantityLength q1 = new QuantityLength(value1, unit1);
+        QuantityLength q2 = new QuantityLength(value2, unit2);
 
-        return length1.equals(length2);
+        return q1.equals(q2);
     }
 
     public static void main(String[] args) {
 
-        System.out.println("Input: 12 inch and 1 foot");
-        System.out.println("Output: Equal (" +
-                checkEquality(12.0, LengthUnit.INCH, 1.0, LengthUnit.FEET) + ")");
+        System.out.println("1 yard == 3 feet : " +
+                checkEquality(1.0, LengthUnit.YARD, 3.0, LengthUnit.FEET));
 
-        System.out.println();
+        System.out.println("1 yard == 36 inches : " +
+                checkEquality(1.0, LengthUnit.YARD, 36.0, LengthUnit.INCH));
 
-        System.out.println("Input: 1 foot and 1 foot");
-        System.out.println("Output: Equal (" +
-                checkEquality(1.0, LengthUnit.FEET, 1.0, LengthUnit.FEET) + ")");
+        System.out.println("12 inches == 1 foot : " +
+                checkEquality(12.0, LengthUnit.INCH, 1.0, LengthUnit.FEET));
 
-        System.out.println();
+        System.out.println("30.48 cm == 1 foot : " +
+                checkEquality(30.48, LengthUnit.CENTIMETER, 1.0, LengthUnit.FEET));
 
-        System.out.println("Input: 1 inch and 1 inch");
-        System.out.println("Output: Equal (" +
-                checkEquality(1.0, LengthUnit.INCH, 1.0, LengthUnit.INCH) + ")");
+        System.out.println("2.54 cm == 1 inch : " +
+                checkEquality(2.54, LengthUnit.CENTIMETER, 1.0, LengthUnit.INCH));
     }
 }
